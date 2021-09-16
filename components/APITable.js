f2a2f295c076360e5a892388d1ed3eb32b80d39c
naurinah <87181806+naurinah@users.noreply.github.com>
2021-09-16 23:17:49 +0500

    Update APITable.js

--- components/APITable.js
+++ components/APITable.js
@@ -191,12 +191,12 @@ export default function APITable({ reload, setReload }) {
   
   const fetchApiDetails = async () => {
     if (location.protocol !== 'http:') {
-    location.replace(`http:${location.href.substring(location.protocol.length)}`);
+    location.replace(`http:${location.href.substring(location.protocol.length)}`)}
     const response = await fetch(
       "http://benefitx.blue-ex.com/api/customerportal/api_details.php"
     ).then((res) => res.json());
     setApis(response.details);
-  }
+ 
     
 //   const url = window.location.href
 //   const arr = url.split("/")
